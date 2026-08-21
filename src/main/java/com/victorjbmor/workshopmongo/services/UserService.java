5abad1a990cfbb71d0945170d3c8e7dac18456f5
victorjbmor <victorjbmoraes@gmail.com>
2026-08-21 14:24:25 -0300

    Obtendo um usuario por id

--- src/main/java/com/victorjbmor/workshopmongo/services/UserService.java
+++ src/main/java/com/victorjbmor/workshopmongo/services/UserService.java
@@ -1,12 +1,13 @@
 package com.victorjbmor.workshopmongo.services;
 
 import java.util.List;
-
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
 import com.victorjbmor.workshopmongo.domain.User;
 import com.victorjbmor.workshopmongo.repository.UserRepository;
+import com.victorjbmor.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -19,5 +20,14 @@ public class UserService {
 	
 	public List<User> findAll() {
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+
+	    Optional<User> user = repo.findById(id);
+	    if (user.isEmpty()) {
+	        throw new ObjectNotFoundException("Objeto não encontrado");
+	    }
+	    return user.get();
 	}
 }
