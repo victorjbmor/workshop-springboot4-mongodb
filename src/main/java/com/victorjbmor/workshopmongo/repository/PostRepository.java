9d8fe77bd2c024b295d368ecb74c1c6356b56a73
victorjbmor <victorjbmoraes@gmail.com>
2026-08-24 16:03:02 -0300

    Consulta simples com @Query

--- src/main/java/com/victorjbmor/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/victorjbmor/workshopmongo/repository/PostRepository.java
@@ -3,10 +3,15 @@ package com.victorjbmor.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import com.victorjbmor.workshopmongo.domain.Post;
 
 public interface PostRepository extends MongoRepository <Post, String>{
 	
 	List<Post> findByTitleContainingIgnoreCase(String text); 
+	
+	
+	@Query("{ 'title': {$regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
 }
